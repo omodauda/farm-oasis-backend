d254872a5cdd1e3d1db7f85943c2c76ab1de4dae
omodauda <omodauda.dl@gmail.com>
2021-06-27 11:51:31 +0100

    email service

---
+++ src/services/email/index.js
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer';
+
+const sendMail = async (receipient, subject, html) => {
+  const email = process.env.NODEMAILER_EMAIL;
+  const password = process.env.NODEMAILER_PASSWORD;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: email,
+      pass: password,
+    },
+  });
+
+  const mailOptions = {
+    from: `Farm oasis <${process.env.NODEMAILER_EMAIL}>`,
+    to: receipient,
+    subject,
+    html,
+  };
+
+  // eslint-disable-next-line consistent-return
+  transporter.sendMail(mailOptions, (err, response) => {
+    if (err) {
+      return err;
+    } if (response) {
+      return response;
+    }
+  });
+};
+
+const sendVerificationEmail = async (email, token) => {
+  try {
+    const receipient = email;
+    const subject = 'Welcome to Farm Oasis! Please Confirm Your Email';
+    const html = `
+            Your confirmation token: ${token}
+        `;
+    await sendMail(receipient, subject, html);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default sendVerificationEmail;
