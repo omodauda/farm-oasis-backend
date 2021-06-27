c67006caf7dd6202b51f18d8acc2fce4dbd8a9c8
omodauda <omodauda.dl@gmail.com>
2021-06-27 20:43:26 +0100

    auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+import jwtr from '../utils/jwtr';
+import { errorMsg } from '../utils/response';
+import { Auth } from '../database/models';
+
+// eslint-disable-next-line consistent-return
+export default async function authenticate(req, res, next) {
+  if (!req.headers.authorization) {
+    return errorMsg(res, 401, 'unauthorized request');
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = await jwtr.verify(token, process.env.JWT_SECRET);
+    req.user = await Auth.findByPk(decoded.sub);
+    next();
+  } catch (error) {
+    return errorMsg(res, 500, 'internal server error');
+  }
+}
