beeeeb8463efb13d82be8ca4dc0fd1ee6162188d
omodauda <omodauda.dl@gmail.com>
2022-08-18 12:29:00 +0100

    feat: update confirm token

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -2,9 +2,16 @@ import bcrypt from 'bcryptjs';
 import { sequelize, Auth, User } from '../../database/models';
 import UserService from '../../services/user';
 import generateReferralCode from '../../helpers/referralCode';
-import { signToken, signRefreshToken, regenerateAccessToken } from '../../helpers/jwt';
+import {
+  signToken,
+  signRefreshToken,
+  regenerateAccessToken,
+} from '../../helpers/jwt';
 import { successMsg, errorMsg } from '../../utils/response';
-import { sendVerificationEmail, sendResetTokenEmail } from '../../services/email';
+import {
+  sendVerificationEmail,
+  sendResetTokenEmail,
+} from '../../services/email';
 
 export default class UserController {
   static async createUser(req, res) {
@@ -20,18 +27,25 @@ export default class UserController {
         return errorMsg(res, 400, `email ${email} already exist`);
       }
 
-      const confirmToken = Math.floor(Math.random() * 1000000) + 1;
+      // const confirmToken = Math.floor(Math.random() * 1000000) + 1;
+      const confirmToken = 123456;
       const referralCode = await generateReferralCode(firstName, lastName);
 
       const result = await sequelize.transaction(async (t) => {
-        const auth = await Auth.create({ email, password, confirmToken }, { transaction: t });
-        const user = await User.create({
-          authId: auth.id,
-          firstName,
-          lastName,
-          phone,
-          referralCode,
-        }, { transaction: t });
+        const auth = await Auth.create(
+          { email, password, confirmToken },
+          { transaction: t },
+        );
+        const user = await User.create(
+          {
+            authId: auth.id,
+            firstName,
+            lastName,
+            phone,
+            referralCode,
+          },
+          { transaction: t },
+        );
 
         await sendVerificationEmail(email, confirmToken);
 
@@ -51,7 +65,7 @@ export default class UserController {
           referralCode: user.referralCode,
         };
 
-        return successMsg(res, 201, 'user registered sucessfully', data);
+        return successMsg(res, 201, 'user registered successfully', data);
       });
       return result;
     } catch (error) {
@@ -69,9 +83,12 @@ export default class UserController {
         return errorMsg(res, 400, `${email} is not registered`);
       }
 
-      const isValidPassword = await bcrypt.compare(password, emailExist.password);
+      const isValidPassword = await bcrypt.compare(
+        password,
+        emailExist.password,
+      );
       if (!isValidPassword) {
-        return errorMsg(res, 400, 'invaild password');
+        return errorMsg(res, 400, 'invalid password');
       }
 
       const { id: authId, isAdmin, isVerified } = emailExist;
@@ -149,13 +166,20 @@ export default class UserController {
         return errorMsg(res, 400, 'user is already verified');
       }
       const newConfirmToken = Math.floor(Math.random() * 1000000) + 1;
-      await Auth.update({ confirmToken: newConfirmToken }, {
-        where: {
-          id,
+      await Auth.update(
+        { confirmToken: newConfirmToken },
+        {
+          where: {
+            id,
+          },
         },
-      });
+      );
       await sendVerificationEmail(email, newConfirmToken);
-      return successMsg(res, 200, 'check your email for new confirmation token');
+      return successMsg(
+        res,
+        200,
+        'check your email for new confirmation token',
+      );
     } catch (error) {
       return errorMsg(res, 500, 'internal server error');
     }
@@ -170,11 +194,14 @@ export default class UserController {
         return errorMsg(res, 400, `email ${email} is not registered`);
       }
       const resetToken = Math.floor(Math.random() * 1000000) + 1;
-      await Auth.update({ resetToken }, {
-        where: {
-          email: emailExist.email,
+      await Auth.update(
+        { resetToken },
+        {
+          where: {
+            email: emailExist.email,
+          },
         },
-      });
+      );
       await sendResetTokenEmail(emailExist.email, resetToken);
       return successMsg(res, 200, 'check your email for password reset token');
     } catch (error) {
@@ -194,11 +221,14 @@ export default class UserController {
       const salt = await bcrypt.genSalt(10);
       const hashedNewPassword = await bcrypt.hash(newPassword, salt);
 
-      await Auth.update({ password: hashedNewPassword }, {
-        where: {
-          email,
+      await Auth.update(
+        { password: hashedNewPassword },
+        {
+          where: {
+            email,
+          },
         },
-      });
+      );
       return successMsg(res, 200, 'password reset successful');
     } catch (error) {
       return errorMsg(res, 500, 'internal server error');
